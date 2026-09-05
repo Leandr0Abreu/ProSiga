d744e09b87daed65a72317753873d59276643a19
Leandr0Abreu <leandro.gabriel.abrel.silva@gmail.com>
2026-09-05 18:18:47 +0000

    Adicionando Criar Conta

--- ProSiga/web/src/App.tsx
+++ ProSiga/web/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage.tsx';
+import CreateAccount from './components/Account.tsx';
 
 function App(){
   return(
-    <>
-    <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/criar-conta" element={<CreateAccount />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ ProSiga/web/src/components/Account.tsx
@@ -0,0 +1,17 @@
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Criação de Conta</title>
+
+    <h1>Criação de Conta</h1>
+
+</head>
+<body>
+    
+</body>
+</html>
+
+
+export default CreateAccount;
